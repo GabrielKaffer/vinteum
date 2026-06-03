54d4ccb14932493a25c15d6f46dc7c0eb8a437bd
GabrielKaffer <88665413+GabrielKaffer@users.noreply.github.com>
2026-06-03 20:12:22 -0300

    Update Monte.java

--- vinteum/src/main/java/com/example/Monte.java
+++ vinteum/src/main/java/com/example/Monte.java
@@ -1,17 +1,29 @@
 package com.example;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Monte {
     private List<Carta> cartas = new ArrayList<>();
 
-    public void embaralhar(){
-
+    public Monte() {
+        cartas = new ArrayList<>();
+        for (Naipe naipe : Naipe.values()) {
+            for (int numero = 1; numero <= 10; numero++) {
+                cartas.add(new Carta(numero, naipe));
+            }
+        }
+        embaralhar();
 
     }
 
-    public Carta virar(){
+    public void embaralhar(){
+        Collections.shuffle(cartas);
+
+    }
+
+    public Carta virar( ){
 
     }
 
